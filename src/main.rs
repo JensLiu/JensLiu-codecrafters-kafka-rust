89eb9ca3358e5978dc1ef94e696c7da376d49beb
jensliu <jensliu29@outlook.com>
2025-03-15 19:44:40 +0800

    send correlation 1d

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,43 @@
+use std::io::{Read, Write};
 use std::net::TcpListener;
 
-fn main() {
+struct Response<H> {
+    message_size: i32,
+    header: H,
+    // body:
+}
+struct ResponseHeaderV0 {
+    correlation_id: i32,
+}
+
+type ResponseV0 = Response<ResponseHeaderV0>;
+
+fn main() -> anyhow::Result<()> {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
 
     // Uncomment this block to pass the first stage
 
-    let listener = TcpListener::bind("127.0.0.1:9092").unwrap();
+    let listener = TcpListener::bind("127.0.0.1:9092")?;
 
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
+            Ok(mut stream) => {
                 println!("accepted new connection");
+                // let response = ResponseV0 {
+                //     message_size: 0,
+                //     header: ResponseHeaderV0 {
+                //         correlation_id: 7   // hard coded for now
+                //     }
+                // };
+                let buf = [0, 0, 0, 0, 0, 0, 0, 7]; // hard-coded resposne
+                stream.write(&buf)?;
             }
             Err(e) => {
                 println!("error: {}", e);
             }
         }
     }
+
+    Ok(())
 }
