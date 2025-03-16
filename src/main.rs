d0702953e72a22dac6718ad3106e8debde42e562
jensliu <jensliu29@outlook.com>
2025-03-16 11:34:22 +0800

    refactored

--- src/main.rs
+++ src/main.rs
@@ -1,50 +1,13 @@
+mod network_req_resp;
+
 use std::io::{Cursor, Read, Write, Seek};
 use std::net::TcpListener;
-use anyhow::bail;
 use bytes::{Buf, BufMut};
-use crate::ErrorCode::UnsupportedVersion;
-use crate::RequestApiKey::{ApiVersions, Produce};
+use crate::network_req_resp::*;
 
-struct Response<H> {
-    message_size: i32,
-    header: H,
-    // body:
-}
-struct ResponseHeaderV0 {
-    correlation_id: i32,
-}
 
-struct RequestHeaderV2 {
-    request_api_key: i16,
-    request_api_version: i16,
-    correlation_id: i32,
-    client_id: String,
-}
 
-#[derive(Debug, Eq, PartialEq)]
-enum RequestApiKey {
-    Produce = 0,
-    Fetch = 1,
-    ApiVersions = 18
-}
-
-impl TryFrom<i32> for RequestApiKey {
-    type Error = anyhow::Error;
-
-    fn try_from(value: i32) -> Result<Self, Self::Error> {
-        match value {
-            18 => Ok(Produce),
-            _ => Err(bail!("Invalid RequestApiKey value: {}", value))
-        }
-    }
-}
-
-enum ErrorCode {
-    NoError = 0,
-    UnsupportedVersion = 35
-}
-
-type ResponseV0 = Response<ResponseHeaderV0>;
+// type ResponseV0 = Response<ResponseHeaderV0>;
 
 fn main() -> anyhow::Result<()> {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -58,74 +21,57 @@ fn main() -> anyhow::Result<()> {
         match stream {
             Ok(mut stream) => {
                 println!("accepted new connection");
-                let mut header = [0u8; 4];
-                stream.read_exact(&mut header)?;
-                let message_size = i32::from_be_bytes(header);
-                let mut request = vec![0u8; message_size as usize];
-                stream.read_exact(&mut *request).unwrap();
-                {
-                    let req = header.iter().chain(request.as_slice().iter()).map(|&x| format!("{:x}", x)).collect::<Vec<String>>().join("");
-                    println!("Received request {:?}", req);
-                }
-                let mut cur = request.as_slice();
-                // request header
-                let request_api_key = cur.get_i16();
-                let request_api_version = cur.get_i16();
-                let correlation_id = cur.get_i32();
-                let client_id_len = cur.get_i16();
-                let client_id = if client_id_len != -1 {
-                    let mut client_id = vec![0u8; client_id_len as usize];
-                    cur.read_exact(&mut client_id).unwrap();
-                    String::from_utf8(client_id)?
-                } else {
-                    "".into()
+                let mut request_buf= Vec::new();
+                stream.read_to_end(&mut request_buf)?;
+                let mut cur = Cursor::new(&*request_buf);
+                let request = Request::parse(&mut cur).unwrap();
+                println!("{:?}", request);
+                let response_header = ResponseHeaderV0 {
+                    correlation_id: request.header.correlation_id,
                 };
-                println!("request_api_key={}", request_api_key);
-                println!("request_api_version={}", request_api_version);
-                println!("correlation_id={}", correlation_id);
-                println!("client_id_len={}", client_id_len);
-                println!("client_id={}", client_id);
-                // request
-                let request_key = RequestApiKey::try_from(request_api_key as i32).unwrap();
-                if request_key == RequestApiKey::ApiVersions {
-                    // let client_software_name =
-                }
-                // response
-                let mut data = Vec::new();
-                data.put_i32(correlation_id);       // correlation_id
-
-                if request_key == ApiVersions {
-                    // error code
-                    if request_api_version < 0 || request_api_version > 4 {
-                        println!("Invalid request_api_version={}", request_api_version);
-                        data.put_i16(UnsupportedVersion as i16)
-                    } else {
-                        data.put_i16(ErrorCode::NoError as i16);
+                let response_body = match request.header.request_api_key {
+                    RequestApiKey::Produce => ResponseBody::Empty,
+                    RequestApiKey::Fetch => ResponseBody::Empty,
+                    RequestApiKey::ApiVersions => {
+                        let error_code = if request.header.request_api_version < 0 || request.header.request_api_version > 4 {
+                            ErrorCode::UnsupportedVersion
+                        } else {
+                            ErrorCode::NoError
+                        };
+                        let api_versions = APIKey {
+                            api_key: RequestApiKey::ApiVersions as i16,
+                            min_version: 0,
+                            max_version: 4,
+                        };
+                        let api_keys = vec![api_versions];
+                        ResponseBody::APIVersionsResponseBodyV4(
+                            APIVersionsResponseBodyV4 {
+                                error_code,
+                                api_keys,
+                                throttle_time_ms: 420
+                            }
+                        )
                     }
-                    data.put_i8(2); // array end index
-                    // API_VERSION API
-                    data.put_i16(ApiVersions as i16);   // api key
-                    data.put_i16(0);    // min version
-                    data.put_i16(4);    // max version
-                    data.put_i8(0);     // TAG_BUFFER
-                    data.put_i32(0);    // throttle time in ms?
-                    data.put_i8(0);     // TAG_BUFFER
-                    // possible other APIs
-                }
+                };
 
-                println!("Response data = {:?}", data);
+                let response = Response {
+                    header: response_header,
+                    body: response_body
+                };
 
-                let mut response = Vec::new();
-                response.put_i32(data.len() as i32);    // data length
-                response.put(&data[..]);            // data
-                stream.write_all(&*response).unwrap();
-                println!("written {:?}", response);
+                println!("{:?}", response);
+
+                let mut response_buf = Vec::new();
+                response.write(&mut response_buf);
+                stream.write_all(&*response_buf).expect("TODO: panic message");
             }
-            Err(e) => {
-                println!("error: {}", e);
-            }
+
+
+        Err(e) => {
+            println!("error: {}", e);
         }
     }
+}
 
-    Ok(())
+Ok(())
 }
---
+++ src/network_req_resp.rs
@@ -0,0 +1,188 @@
+use std::hash::Hasher;
+use std::io::{Cursor, Read, Write};
+use anyhow::{anyhow, bail};
+use bytes::{Buf, BufMut};
+use byteorder::WriteBytesExt;
+
+#[derive(Debug, Eq, PartialEq, Copy, Clone)]
+pub(crate) enum RequestApiKey {
+    Produce = 0,
+    Fetch = 1,
+    ApiVersions = 18
+}
+
+impl TryFrom<i16> for RequestApiKey {
+    type Error = anyhow::Error;
+
+    fn try_from(value: i16) -> Result<Self, Self::Error> {
+        match value {
+            18 => Ok(RequestApiKey::ApiVersions),
+            _ => Err(bail!("Invalid RequestApiKey value: {}", value))
+        }
+    }
+}
+
+#[derive(Debug, Copy, Clone)]
+pub(crate) enum ErrorCode {
+    NoError = 0,
+    UnsupportedVersion = 35,
+    WTF = -1,
+}
+
+#[derive(Debug)]
+enum RequestBody {
+    Empty,
+}
+
+#[derive(Debug)]
+pub(crate) struct Request {
+    pub(crate) message_size: i32,
+    pub(crate) header: RequestHeaderV2,
+    pub(crate) body: RequestBody,
+}
+
+#[derive(Debug)]
+pub(crate) struct RequestHeaderV2 {
+    pub(crate) request_api_key: RequestApiKey,
+    pub(crate) request_api_version: i16,
+    pub(crate) correlation_id: i32,
+    pub(crate) client_id: String,
+}
+
+impl RequestHeaderV2 {
+    fn parse(cur: &mut Cursor<&[u8]>) -> anyhow::Result<Self> {
+        let request_api_key = cur.get_i16();
+        let request_api_version = cur.get_i16();
+        let correlation_id = cur.get_i32();
+        let client_id = parse_nullable_string(cur)?;
+        Ok(Self {
+            request_api_key: RequestApiKey::try_from(request_api_key)?,
+            request_api_version,
+            correlation_id,
+            client_id
+        })
+    }
+}
+
+impl RequestBody {
+    fn parse(cur: &mut Cursor<&[u8]>, request_api_version: i16, request_api_key: RequestApiKey) -> anyhow::Result<Self, ErrorCode> {
+        match request_api_key {
+            RequestApiKey::ApiVersions => {
+                // if request_api_version < 0 || request_api_version > 4 {
+                //     Err(ErrorCode::UnsupportedVersion)?;
+                // }
+                Ok(RequestBody::Empty)
+            },
+            _ => Err(ErrorCode::WTF)
+        }
+    }
+}
+
+impl Request {
+    pub fn parse(cur: &mut Cursor<&[u8]>) -> Result<Self, ErrorCode> {
+        let message_size = cur.get_i32();
+        let mut buf = vec![0u8; message_size as usize];
+        cur.read_exact(&mut *buf).expect("TODO: panic message");
+        let mut cur_body = Cursor::new(&*buf);
+        let Ok(header) = RequestHeaderV2::parse(&mut cur_body) else {
+            Err(ErrorCode::WTF)?
+        };
+        let body = RequestBody::parse(&mut cur_body, header.request_api_version, header.request_api_key)?;
+        Ok(Self {
+            message_size, header, body
+        })
+    }
+}
+enum ResponseHeader {
+    ResponseHeaderV0(ResponseHeaderV0)
+}
+
+#[derive(Debug)]
+pub(crate) struct ResponseHeaderV0 {
+    pub(crate) correlation_id: i32,
+}
+
+#[derive(Debug)]
+pub(crate) enum ResponseBody {
+    Empty,
+    APIVersionsResponseBodyV4(APIVersionsResponseBodyV4)
+}
+
+#[derive(Debug)]
+pub(crate) struct Response {
+    pub(crate) header: ResponseHeaderV0,
+    pub(crate) body: ResponseBody,
+}
+
+#[derive(Debug)]
+pub(crate) struct APIKey {
+    pub(crate) api_key: i16,
+    pub(crate) min_version: i16,
+    pub(crate) max_version: i16,
+}
+#[derive(Debug)]
+pub(crate) struct APIVersionsResponseBodyV4 {
+    pub(crate) error_code: ErrorCode,
+    pub(crate) api_keys: Vec<APIKey>,
+    pub(crate) throttle_time_ms: i32
+}
+
+impl ResponseHeaderV0 {
+    fn write(&self, buf: &mut Vec<u8>) {
+        buf.put_i32(self.correlation_id);
+    }
+}
+
+impl APIVersionsResponseBodyV4 {
+    fn write(&self, buf: &mut Vec<u8>) {
+        buf.write_i16::<byteorder::BigEndian>(self.error_code as i16).expect("TODO: panic message");
+        for api_key in &self.api_keys {
+            buf.put_i16(api_key.api_key);
+            buf.put_i16(api_key.min_version);
+            buf.put_i16(api_key.max_version);
+            put_tag_buffer(buf);
+        }
+        buf.put_i32(self.throttle_time_ms);
+        put_tag_buffer(buf);
+    }
+}
+
+impl ResponseBody {
+    fn write(&self, cur: &mut Vec<u8>) {
+        match self {
+            ResponseBody::APIVersionsResponseBodyV4(body) => {
+                body.write(cur)
+            }
+            _ => {}
+        }
+    }
+}
+
+impl Response {
+    pub fn write(&self, buf: &mut Vec<u8>) {
+        let mut header_buf = Vec::new();
+        self.header.write(&mut header_buf);
+        let mut body_buf = Vec::new();
+        self.body.write(&mut body_buf);
+        let message_size = header_buf.len() + body_buf.len();
+        buf.put_i32(message_size as i32);
+        buf.put(header_buf.as_slice());
+        buf.put(body_buf.as_slice());
+    }
+}
+
+
+
+fn parse_nullable_string(cur: &mut Cursor<&[u8]>) -> anyhow::Result<String> {
+    let size = cur.get_i16();
+    let str = if size == -1 { "".into() } else {
+        let mut s = vec![0u8; size as usize];
+        cur.read_exact(&mut *s)?;
+        String::from_utf8(s.to_vec())?
+    };
+    Ok(str)
+}
+
+fn put_tag_buffer(buf: &mut Vec<u8>) {
+    buf.put_i8(0);
+}
